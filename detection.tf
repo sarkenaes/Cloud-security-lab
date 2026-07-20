38fec021a3a04449ebaeb8c6cdfc4f00f8350f47
sarkenaes <sarkenaes@gmail.com>
2026-07-20 18:41:04 -0500

    Added cloudtrail and guardwatch

--- detection.tf
+++ detection.tf
@@ -40,4 +40,41 @@ resource "aws_cloudtrail" "cloud-trail" {
   s3_bucket_name = aws_s3_bucket.cloudtrail_logs.id
   include_global_service_events = true
   is_multi_region_trail =true
+  cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.cloudtrail_logs.arn}:*"
+  cloud_watch_role_group_arn=aws_iam_role.cloudtrail_cloudwatch.arn
+}
+##Creates the guard duty resource
+resource "aws_guardduty_detection" "main"{
+  enable ="true"
+}
+resource "aws_cloudwatch_log_group" "cloudtrail_logs" {
+  name ="/aws/cloudtrail/${var.project_name}"
+  retention_in_days =90
+}
+resource "aws_iam_role" "cloudtrail_cloudwatch"{
+name ="${var.project_name}-cloudtrail-cw-role"
+assume_role_policy =jsonencode({
+  Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "cloudtrail.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+})
+}
+resource "aws_iam_role_policy" "cloudtrail_cloudwatch_policy"{
+  name ="${var.project_name}-cloudtrail-cw-policy"
+  role=aws_iam_role.cloudtrail_cloudwatch.id
+  policy =jsonencode({
+      Version ="2012-10-17"
+      Statement=[{
+        Effect ="Allow"
+        Action =[
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "${aws_cloudwatch_log_group.cloudtrail_logs.arn}:*"
+      }]
+
+  })
 }
